5f60465b9cd6c1f3fe6960aaee1b54fe9eaba545
Ruipeng Hao <haoruipeng@126.com>
2023-06-20 22:34:33 +0800

    initial commit

---
+++ contracts/RandomIpfsNFT.sol
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+error RandomIpfsNft__AlreadyInitialized();
+error RandomIpfsNft__NeedMoreETHSent();
+error RandomIpfsNft__RangeOutOfBounds();
+error RandomIpfsNft__TransferFailed();
+
+contract RandomIpfsNFT is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
+    //when we mint an NFT, we will trigger a Chainlink VRF call to get a random number
+    //using that number, we will get a random NFT
+    //Pug : super rare
+    //Shiba : sort of rare
+    //Bernard : common
+
+    // Types
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
+
+    //chainlink VRF variable
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    //VRF helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    //NFT
+    uint256 public s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    string[] internal s_dogTokenUris;
+    uint256 internal i_mintFee;
+    bool private s_initialized;
+
+    // Events
+    event NftRequested(uint256 indexed requestId, address requester);
+    event NftMinted(Breed breed, address minter);
+
+    constructor(
+        address vrfCoordinator,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit,
+        string[3] memory dogTokenUris,
+        uint256 mintFee
+    ) VRFConsumerBaseV2(vrfCoordinator) ERC721("Random IPFS NFT", "HAHA") {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+        s_dogTokenUris = dogTokenUris;
+        i_mintFee = mintFee;
+        _initializeContract(dogTokenUris);
+        s_tokenCounter = 0;
+    }
+
+    //users have to pay to mint an NFT
+    //the owner of the contract can withdraw the ETH
+
+    function requestNFT() public payable returns (uint256 requestId) {
+        if (msg.value < i_mintFee) {
+            revert RandomIpfsNft__NeedMoreETHSent();
+        }
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        s_requestIdToSender[requestId] = msg.sender;
+        emit NftRequested(requestId, msg.sender);
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        address dogOwner = s_requestIdToSender[requestId];
+        uint256 newTokenId = s_tokenCounter;
+
+        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
+        Breed breed = getBreedFromModdedRng(moddedRng);
+        s_tokenCounter += 1;
+        _safeMint(dogOwner, newTokenId);
+        _setTokenURI(newTokenId, s_dogTokenUris[uint256(breed)]);
+        emit NftMinted(breed, dogOwner);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        if (!success) {
+            revert RandomIpfsNft__TransferFailed();
+        }
+    }
+
+    function _initializeContract(string[3] memory dogTokenUris) private {
+        if (s_initialized) {
+            revert RandomIpfsNft__AlreadyInitialized();
+        }
+        s_dogTokenUris = dogTokenUris;
+        s_initialized = true;
+    }
+
+    function getBreedFromModdedRng(
+        uint256 moddedRng
+    ) public pure returns (Breed) {
+        uint256 cumulativeSum = 0;
+        uint256[3] memory chanceArray = getChanceArray();
+        for (uint256 i = 0; i < chanceArray.length; i++) {
+            // Pug = 0 - 9  (10%)
+            // Shiba-inu = 10 - 39  (30%)
+            // St. Bernard = 40 = 99 (60%)
+            if (moddedRng >= cumulativeSum && moddedRng < chanceArray[i]) {
+                return Breed(i);
+            }
+            cumulativeSum = chanceArray[i];
+        }
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        return [10, 40, MAX_CHANCE_VALUE];
+    }
+
+    //不用下面这个被注释掉的tokenURI(uint256)了，因为有了_setTokenURI()
+    //function tokenURI(uint256) public view override returns (string memory) {}
+
+    function getMintFee() public view returns (uint256) {
+        return i_mintFee;
+    }
+
+    function getDogTokenUris(
+        uint256 index
+    ) public view returns (string memory) {
+        return s_dogTokenUris[index];
+    }
+
+    function getInitialized() public view returns (bool) {
+        return s_initialized;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+    receive() external payable {}
+}
---
+++ deploy/03-deploy-dynamic-svg.js
@@ -0,0 +1,39 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const fs = require("fs")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let ethUsdPriceFeedAddress
+
+    if (chainId == 31337) {
+        // Find ETH/USD price feed
+        const EthUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = EthUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
+    }
+
+    const lowSVG = fs.readFileSync("./images/dynamicNFT/frown.svg", { encoding: "utf8" })
+    const highSVG = fs.readFileSync("./images/dynamicNFT/happy.svg", { encoding: "utf8" })
+
+    log("----------------------------------------------------")
+    arguments = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    const dynamicSvgNft = await deploy("DynamicSvgNFT", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(dynamicSvgNft.address, arguments)
+    }
+}
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
